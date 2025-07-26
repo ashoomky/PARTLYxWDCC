1ba3ea712dde43c41a823fe7d7733b997f68bd2e
sonjali4 <sonjali2006@gmail.com>
2025-07-27 11:04:10 +1200

    change text btn

---
+++ main.js
@@ -0,0 +1,8 @@
+let textIndex = 0;
+const btn = document.getElementById("splitButton");
+
+function changeText() {
+    const text = ["show half car", "show full car"];
+    textIndex = (textIndex + 1) % 2;
+    btn.innerHTML = text[textIndex];
+}
