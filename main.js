f934cbb44d398b7adf19228daa09784f8391fa8e
sonjali4 <sonjali2006@gmail.com>
2025-07-27 13:01:21 +1200

    updated main.js

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const imgArray = ["./images/barbie_main_ver2.png",
     ["./images/barbie_half_ver2.png", ["wheel.png", "engine.png", [part1, part2]]]
 ];
 
-const carImg = document.getElementById("fullCar");
+let carImg = document.getElementById("fullCar");
 carImg.addEventListener('click', zoom);
 
 function zoom() {
     carImg.src = "./images/barbie_half_ver2.png";
+    let carImg = document.getElementById("fullCar");
 }
 
 // info card hover
